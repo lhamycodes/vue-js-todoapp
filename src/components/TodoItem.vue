22f1a79dfd81ddb58cf14d749ebf1801d94a2634
lhamide <flashpointintl@gmail.com>
2019-02-16 13:14:12 +0100

    Complete TodoApp

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="todoitem" v-bind:class="{isComplete:todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{ todo.title }}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "TodoItem",
+    props : ["todo"],
+    methods : {
+        markComplete() {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todoitem {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+    .isComplete {
+        text-decoration: line-through;
+    }
+    .del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the About Page. It is Part of the Vue Crash Course by Brad Traversy on Youtube</p>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    text-align: center
+  }
+  div > h1 {
+    margin-top: 10px;
+  }
+</style>
